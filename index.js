b2030cf74bfd96dd011f77f5832f9130e347d917
NIKITA AD <nikita8ad@gmail.com>
2018-07-24 10:46:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function theBeatlesPlay(music, instr){
-  var result[];
+  var result = [];
+  for(var i=0; i<result.length; i++){
+    var str = music[i] + " plays " + instr[i];
+    result.push(str);
+  }
+  return result;
+}
+
+function johnLennonFacts(arr){
+  
 }
