53bd67deaf809c83dac1aa61453701091de403b5
NIKITA AD <nikita8ad@gmail.com>
2018-07-24 11:14:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,9 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(num){
   var arr = [];
-  
+  do{
+    arr.push("I love the Beatles!");
+    num++;
+  }while(num < 15);
+  return arr;
 }
